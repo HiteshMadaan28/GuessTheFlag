dbdf20ce8ac8ff3f0d4dabf0601aa5e1aaa738e3
HiteshMadaan28 <hiteshmadaan28@gmail.com>
2024-03-04 20:19:31 +0530

    Initial Commit

---
+++ GuessTheFlag/GuessTheFlagApp.swift
@@ -0,0 +1,17 @@
+//
+//  GuessTheFlagApp.swift
+//  GuessTheFlag
+//
+//  Created by Hitesh Madaan on 04/03/24.
+//
+
+import SwiftUI
+
+@main
+struct GuessTheFlagApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
